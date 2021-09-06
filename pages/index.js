07eb3ea84c58e43f97c83b53e022611c7ed4b19d
Behrouz <bmb.mm7@gmail.com>
2021-09-06 09:48:04 +0430

    add static

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Layout from '../components/Layout'
 
-const Index = () => (
-  <Layout title="Home">
+const Index = (props) => {
+  console.log(props.sales)
+ return <Layout title="Home">
     <p>Welcome to the home page</p>
   </Layout>
-)
+}
+
+export async function getStaticProps(){
+  const response = await fetch('https://nextjs-course-562c0-default-rtdb.firebaseio.com/sales.json')
+  const data = await response.json()
+
+  return{
+    props:{
+      sales:data
+    }
+  }
+}
 
 export default Index
 
